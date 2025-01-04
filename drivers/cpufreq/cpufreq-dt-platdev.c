fb6fe4a5f4aebdab3d1fd938ab8d7ecd9d0ebad1
Daniil Titov <daniilt971@gmail.com>
2025-01-04 19:27:52 +0300

    cpufreq: Block MSM8917 in cpufreq-dt-platdev
    Add MSM8917 to cpufreq-dt-platdev blocklist since the
    actual scaling is handled by the 'qcom-cpr' driver.
    
    Signed-off-by: Daniil Titov <daniilt971@gmail.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -151,6 +151,7 @@ static const struct of_device_id blocklist[] __initconst = {
 	{ .compatible = "qcom,qcm2290", },
 	{ .compatible = "qcom,qcm6490", },
 	{ .compatible = "qcom,qcs404", },
+	{ .compatible = "qcom,msm8917", },
 	{ .compatible = "qcom,qdu1000", },
 	{ .compatible = "qcom,sa8155p" },
 	{ .compatible = "qcom,sa8540p" },
